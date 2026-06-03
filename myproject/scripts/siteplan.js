0c4de3cb35e0a2e6d6ff55705d693b756a94c876
Samuel-Innocent <sinnocent2@byupathway.edu>
2026-06-03 23:54:14 +0100

    Site

---
+++ myproject/scripts/siteplan.js
@@ -0,0 +1,3 @@
+// --- Dynamic Footer Dates for Site Plan ---
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+document.getElementById('lastModified').textContent = `Last Modification: ${document.lastModified}`;
